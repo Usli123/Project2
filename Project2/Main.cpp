084a69a69d1625eb671a0c1b7fcfb2d157809d9c
user1 <hjsvvfis@mail.ru>
2023-05-12 18:32:13 +0300

    Экзамен

--- Project2/Main.cpp
+++ Project2/Main.cpp
@@ -1,38 +1,78 @@
 #include<iostream>
-#include<fstream>
 
 using namespace std;
 
 
-
-bool summ(int mus[], int len, int b) {
-	int s=0;
-	for (int i = 0; i < len; i++) {
-
-		s += mus[i];
-	}
-		if (s == b) {
-			return true;
-		}
-		else {
-			return false;
-		}
-}
-
-
 int main() {
 	setlocale(LC_ALL, "");
-	srand(time(0) * time(0));
+	cout << "Квадрат — правильный четырёхугольник, то есть плоский четырёхугольник, у которого все углы и все стороны равны." << endl;
+	cout << "Представленный калькулятор поможет вам найти следующие параметры квадрата, для этого необходимо ввести сначала чему равна сторона квадрата, а затем соответствующий номер параметра: " << endl;
+	cout << "1-Периметр квадрата" << endl << "2-Площадь квадрата" << endl << "3-Длина диагонали квадрата" << endl << endl;
+	
+	int p;
+	int s;
+	int d;
+	
+	while (true) {
+		int a;
+		cout << "Введите размер длины квадрата: " << endl;
+		cin >> a;
+		bool stop = false;
+		if (a <= 0)
+		{
+			cout << "Некорректное значение" << endl;;
+			continue;
+		}
+		if (stop)
+		{
+			break;
+		}
 
-	int number;
-	cin >> number;
-	int len = 6;
-	int mus[] = { 1,2,3,4,5,6 };
-	if (summ(mus, len, number)) {
-		cout << "true" << endl;
-	}
-	else {
-		cout << "false" << endl;
+
+		while (true) {
+			int action;
+			cin >> action;
+			bool stop = false;
+			if (action > 3||action<1)
+			{
+				cout << "Команда не найдена" << endl;;
+				continue;
+			}
+			if (stop)
+			{
+				break;
+			}
+			switch (action)
+			{
+			case 1:
+				p = 4 * a;
+				cout << "Периметр квадрата: " << p << endl;
+				stop = true;
+				break;
+				if (stop)
+				{
+					break;
+				}
+			case 2:
+				s = a * a;
+				cout << "Площадь квадрата: " << s << endl;
+				stop = true;
+				break;
+				if (stop)
+				{
+					break;
+				}
+			case 3:
+				d = a * sqrt(2);
+				cout << "Длина диагонали квадрата: " << d << endl;
+				stop = true;
+				break;
+				if (stop)
+				{
+					break;
+				}
+			}
+		}
 	}
 
 	return 0;
